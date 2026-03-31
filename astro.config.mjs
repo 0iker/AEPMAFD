8c22603bd118626e9000a5f3910c94cde7409273
0iker <iker.r.s.2002@gmail.com>
2026-03-31 12:46:59 +0200

    Reajuste de imágenes e icono de menú

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import path from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,14 @@ export default defineConfig({
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@components": path.resolve("./src/components"),
+        "@layouts": path.resolve("./src/layouts"),
+        "@styles": path.resolve("./src/styles"),
+        "@assets": path.resolve("./src/assets"),
+        "@icons": path.resolve("./src/icons"),
+      },
+    },
   },
 });
